e3d30e82ddfa62e2bd8436fcba6264d9a27b0ac9
gabrilfranca <164918506+gabrilfranca@users.noreply.github.com>
2025-10-10 14:26:45 -0300

    hh

--- maiin.js
+++ maiin.js
@@ -1,2 +1,11 @@
 constbotoes=document.querySelectorAll(".botao");
 consttexto=document.querySelectorAll("aba-conteudo");
+for (leti=0;<botoes.length;i++){
+    botoes[j].onclick=function(){
+        for (letj=0;j<botoes.length;j++){
+
+        }
+     {
+        
+    }
+}
