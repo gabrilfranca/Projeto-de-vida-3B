16805998dc13046f695e5f5515605979ff23e12e
gabrilfranca <164918506+gabrilfranca@users.noreply.github.com>
2025-10-22 14:48:27 -0300

    hh

--- maiin.js
+++ maiin.js
@@ -1,11 +1,13 @@
-constbotoes=document.querySelectorAll(".botao");
-consttexto=document.querySelectorAll("aba-conteudo");
-for (leti=0;<botoes.length;i++){
+const botoes=document.querySelectorAll(".botao");
+const textos=document.querySelectorAll(".aba-conteudo");
+for (let i = 0;i<botoes.length;i++){
     botoes[j].onclick=function(){
         for (letj=0;j<botoes.length;j++){
-
+        botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
         }
-     {
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
         
     }
 }
